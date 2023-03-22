e4ec428c431b5e523afe36f9f11f67783d3ebae4
Viktor Nagy <viktor.nagy@365.bank>
2023-03-22 11:57:54 +0100

    feat: random and post

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use rocket::serde::json::Json;
+
+use crate::models::Model;
+pub mod models;
+
 #[macro_use]
 extern crate rocket;
 
@@ -6,7 +11,21 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/random")]
+fn random_model() -> Json<Model> {
+    Json(Model {
+        id: 1,
+        name: "haha".to_string(),
+        path: "path".to_string(),
+    })
+}
+
+#[post("/", data = "<model>")]
+fn create_model(model: Json<Model>) -> Json<Model> {
+    model
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, random_model, create_model])
 }
